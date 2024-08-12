19a713bc2a070bd93e793093630ee722dc341557
apengda <damengpeng@163.com>
2024-08-12 17:33:28 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wenote
 基于微信的个人智能笔记
+
+## 
+https://github.com/Wechat-Group/WxJava
+https://github.com/wu2754522801/Aastar-WX-ChatGPT
---
+++ src/main/java/com/github/pdaodao/wenote/WeNoteMain.java
@@ -0,0 +1,7 @@
+package com.github.pdaodao.wenote;
+
+public class WeNoteMain {
+    public static void main(String[] args) {
+        System.out.println("hello wenote.");
+    }
+}
